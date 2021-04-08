3daea561421653f9e00d9604fadc288ab00c06d7
BrianSammit <briansammit.cruz@gmail.com>
2021-04-08 16:20:37 -0500

    Add html bare bones and Style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import "./style.scss";
+
+let loc = document.getElementById("location");
+let tempIcon = document.getElementById("temp-icon");
+let tempValue = document.getElementById("temp-value");
+let climate = document.getElementById("climate");
+let iconFile;
+const searchImput = document.getElementById("search-bar");
+const searchButton = document.getElementById("search-button");
+
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=07dd5cc14ab786daf9016aba357c307f`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+
+        .then((data) => {
+          const { name } = data;
+          const { feels_like } = data.main;
+          const { id, main } = data.weather[0];
+
+          loc.textContent = name;
+          climate.textContent = main;
+          tempValue.textContent = Math.round(feels_like - 273);
+        });
+    });
+  }
+});
