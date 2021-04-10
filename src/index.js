2c17262ce125835dabbef1a6b33363a6d2f886c3
BrianSammit <briansammit.cruz@gmail.com>
2021-04-09 20:41:46 -0500

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,55 +1,54 @@
-import "./style.scss";
+import './style.scss';
 
-let loc = document.getElementById("location");
-let tempIcon = document.getElementById("temp-icon");
-let tempValue = document.getElementById("temp-value");
-let climate = document.getElementById("climate");
-const searchImput = document.getElementById("search-bar");
-const searchButton = document.getElementById("search-button");
-
-searchButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  getWeather(searchImput.value);
-  searchImput.value = "";
-});
+const loc = document.getElementById('location');
+const tempIcon = document.getElementById('temp-icon');
+const tempValue = document.getElementById('temp-value');
+const climate = document.getElementById('climate');
+const searchImput = document.getElementById('search-bar');
+const searchButton = document.getElementById('search-button');
 
 const getWeather = async (city) => {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=07dd5cc14ab786daf9016aba357c307f`,
 
-      { mode: "cors" }
+      { mode: 'cors' },
     );
 
     const weatherData = await response.json();
-    console.log(weatherData);
     const { name } = weatherData;
-    const { feels_like } = weatherData.main;
+    const { temp } = weatherData.main;
     const { id, main } = weatherData.weather[0];
     loc.textContent = name;
     climate.textContent = main;
-    tempValue.textContent = Math.round(feels_like - 273);
+    tempValue.textContent = Math.round(temp - 273);
     if (id < 300 && id > 200) {
-      tempIcon.src = "../src/animated/thunder.svg";
+      tempIcon.src = '../src/animated/thunder.svg';
     } else if (id < 400 && id > 300) {
-      tempIcon.src = "../src/animated/rainy-2.svg";
+      tempIcon.src = '../src/animated/rainy-2.svg';
     } else if (id < 600 && id > 500) {
-      tempIcon.src = "../src/animated/rainy-7.svg";
+      tempIcon.src = '../src/animated/rainy-7.svg';
     } else if (id < 700 && id > 600) {
-      tempIcon.src = "../src/animated/snowy-6.svg";
+      tempIcon.src = '../src/animated/snowy-6.svg';
     } else if (id < 800 && id > 700) {
-      tempIcon.src = "../src/animated/cloudy-day-2.svg";
+      tempIcon.src = '../src/animated/cloudy-day-2.svg';
     } else if (id === 800) {
-      tempIcon.src = "../src/animated/.svg";
+      tempIcon.src = '../src/animated/.svg';
     } else if (id < 900 && id > 800) {
-      tempIcon.src = "../src/animated/cloudy.svg";
+      tempIcon.src = '../src/animated/cloudy.svg';
     }
   } catch (error) {
-    alert("city not found");
+    console.log('Nocity found'); /* eslint-disable-line no-console */
   }
 };
 
-window.addEventListener("load", () => {
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  getWeather(searchImput.value);
+  searchImput.value = '';
+});
+
+window.addEventListener('load', () => {
   let long;
   let lat;
 
@@ -57,40 +56,36 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
 
       const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=07dd5cc14ab786daf9016aba357c307f`;
 
       fetch(api)
-        .then((response) => {
-          return response.json();
-        })
+        .then((response) => response.json())
 
         .then((data) => {
           const { name } = data;
-          const { feels_like } = data.main;
+          const { temp } = data.main;
           const { id, main } = data.weather[0];
 
           loc.textContent = name;
           climate.textContent = main;
-          tempValue.textContent = Math.round(feels_like - 273);
+          tempValue.textContent = Math.round(temp - 273);
           if (id < 300 && id > 200) {
-            tempIcon.src = "../src/animated/thunder.svg";
+            tempIcon.src = '../src/animated/thunder.svg';
           } else if (id < 400 && id > 300) {
-            tempIcon.src = "../src/animated/rainy-2.svg";
+            tempIcon.src = '../src/animated/rainy-2.svg';
           } else if (id < 600 && id > 500) {
-            tempIcon.src = "../src/animated/cloudy-day-2.svg";
+            tempIcon.src = '../src/animated/cloudy-day-2.svg';
           } else if (id < 700 && id > 600) {
-            tempIcon.src = "../src/animated/snowy-6.svg";
+            tempIcon.src = '../src/animated/snowy-6.svg';
           } else if (id < 800 && id > 700) {
-            tempIcon.src = "../src/animated/cloudy-day-2.svg";
+            tempIcon.src = '../src/animated/cloudy-day-2.svg';
           } else if (id === 800) {
-            tempIcon.src = "../src/animated/.svg";
+            tempIcon.src = '../src/animated/.svg';
           } else if (id < 900 && id > 800) {
-            tempIcon.src = "../src/animated/cloudy.svg";
+            tempIcon.src = '../src/animated/cloudy.svg';
           }
-
-          console.log(data);
         });
     });
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.js',
+  mode: 'development',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
     port: 8080,
   },
   module: {
     rules: [
       {
         test: /\.(scss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|gif)$/i,
-        use: ["url-loader"],
+        use: ['url-loader'],
       },
     ],
   },
